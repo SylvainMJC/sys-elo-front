32c5b518d33b14ffc3205ee02c8dfb94401f4991
alex <81435330+Al-vallon@users.noreply.github.com>
2025-05-17 15:30:32 +0200

    update apiUrl in environment.development.ts to use localhost

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://localhost:3000/api',
   //   apiUrl: 'https://jsonplaceholder.typicode.com',
-  apiUrl: 'https://sys-elo-back-y0tl.onrender.com/api',
+  //apiUrl: 'https://sys-elo-back-y0tl.onrender.com/api',
 };
